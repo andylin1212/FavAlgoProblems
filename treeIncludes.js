cbc26cded0801b0b10de0930063c6389c7295d5b
Yen Chen Lin <andylin1212@gmail.com>
2023-01-29 17:23:24 -0500

    implement solution 1 for BFS iteratvie

--- treeIncludes.js
+++ treeIncludes.js
@@ -11,4 +11,27 @@ class Node {
   }
 }
 
-//Solution 1: Breadth first, iterative
+/*Solution 1: Breadth first, iterative
+O(n) time: worst case explore all nodes (in this case .shift will be treated as constant time)
+O(n) space: building a queue, at most all nodes
+*/
+
+function treeIncludes (root, target) {
+  if (root === null) return false;
+
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const curr = queue.shift();  //will be treated as constant time, actually O(n) time each call in javascript
+    //if found target value
+    if (curr.val === target) return true;
+
+    //else, add the non nulls left or right to queue for exploration
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+
+  //if finished explore all nodes, no current nodes with target value, just return false
+  return false;
+}
+
