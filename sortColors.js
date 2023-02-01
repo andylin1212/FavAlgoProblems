54c05e44bed4acefaaaad9e083e9710c44c2ad52
Yen Chen Lin <andylin1212@gmail.com>
2023-01-31 21:05:46 -0500

    sort colors problem setup

---
+++ sortColors.js
@@ -0,0 +1,14 @@
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Concept Check: understand what the problem is asking. Need to move all reds to beginning of list and blue to end of list in place, then white will be sorted too
+*/
+
+//Solution 1:
+//O(n) time:
+//O(1) space:  no extra data structure
+
