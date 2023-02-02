38ab7badfed21cef5f51421030d294e06540dfe8
Yen Chen Lin <andylin1212@gmail.com>
2023-02-02 13:06:06 -0500

    adding description to solution 1

--- uniquePaths.js
+++ uniquePaths.js
@@ -9,22 +9,26 @@ Concept check:
 Since can only move right or down from the start (or each grid), the paths to get to any block on the grid is the summation of paths to get to the block above current block and paths to get to the block to the left of current block.
 
 There would only be one path from all blocks that have same row or column with starting block
-row === 1 || col === 1, that is the base case
+row = m - 1 === 0 || col = n - 1 === 0, that is the base case
 */
 
 //Solution 1: create matrix, dp
 //O(m * n) time: traverse whole matrix
 //O(m * n) space: create matrix
+
 function uniquePaths(m, n) {
-  //create matrix of same size, m and n.
+  //create matrix of same size, m and n of uniquePaths at each block
   //fill all with 1s (important part is 1st row and 1st column are all 1s,initiated others as 1st because we will overwrite eventually)
   let grid = new Array(m).fill().map(()=> new Array(n).fill(1))
 
+  //traverse through matrix starting at r, c = 1, 1
   for (let r = 1; r < m; r++) {
       for (let c = 1; c < n; c++) {
+          //set unqiuePath to summation of paths to get to the block above current block and paths to get to the block to the left of current block
           grid[r][c] = grid[r - 1][c] + grid[r][c - 1]
       }
   }
 
+  //return unique paths of bottom right block
   return grid[m - 1][n - 1]
 };
