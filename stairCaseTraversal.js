38f2e00ed760196e28bed682f1f5ce4033c9a549
Yen Chen Lin <andylin1212@gmail.com>
2023-02-02 14:32:57 -0500

    adding solution 1 recursive with memo and description

--- stairCaseTraversal.js
+++ stairCaseTraversal.js
@@ -5,3 +5,35 @@ For example, if you were given a staircase of height = 3 and maxSteps = 2 you co
 
 Note that maxSteps <= height will always be true
 */
+
+//Solution 1: recursion with memoization
+//O(n * k) time: where n is height and k is maxSteps (if no memoization will be k ^ n time complexity)
+//O(n) space: max recursive calls on stack
+
+function staircaseTraversal(height, maxSteps, visited = {}) {
+  //set base cases
+  if (height in visited) return visited[height]
+  //one way to climb a zero height
+  if (height === 0) return 1;
+  if (height < 0) return 0;
+
+  //initialize count to zero
+  let count = 0;
+
+  //iterate from 1 to max amount of steps you can take
+  for (let i = 1; i <= maxSteps; i++) {
+    //sum up ways to get to all steps that are height - allowed steps
+    count += staircaseTraversal(height - i, maxSteps, visited)
+  }
+
+  //return
+  visited[height] = count;
+  return visited[height]
+}
+
+
+//Solution 2: iterative solution
+function staircaseTraversal(height, maxSteps) {
+  let steps = new Array(height + 1).fill(0)
+
+};
