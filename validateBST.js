56a0710b72544969405ad1d432f4d32fa68d3b06
Yen Chen Lin <andylin1212@gmail.com>
2023-02-08 18:47:35 -0800

    added solution for validateBST with concept check

--- validateBST.js
+++ validateBST.js
@@ -6,10 +6,21 @@ A valid BST is defined as follows:
 The left subtree of a node contains only nodes with keys less than the node's key.
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
+
+Concept check:
+Each BST node has a max and min allowed value, it can be determined by its parent's node's value
 */
 
-function isValidBST (root, max = Infinity, min = -Infinity) {
+//Solution:
+//O(n) time: explore all nodes
+//O(d) space: at most depth recursion calls in stack
 
+function isValidBST (root, max = Infinity, min = -Infinity) {
+  if (root === null) return true;
+
+  if (root.val >= max || root.val <= min) return false
+
+  return isValidBST(root.left, root.val, min) && isValidBST(root.right, max, root.val)
 }
 
 function TreeNode(val, left, right) {
