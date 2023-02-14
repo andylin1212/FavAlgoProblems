25db4ce903c6df368eeed72e40e01b184f55781b
Yen Chen Lin <andylin1212@gmail.com>
2023-02-14 13:57:05 -0500

    solution with time/space complexity with description

--- lowestCommonAncestor.js
+++ lowestCommonAncestor.js
@@ -5,3 +5,19 @@ According to the definition of LCA on Wikipedia: “The lowest common ancestor i
 */
 
 //Solution:
+//O(n) time: visit all nodes in tree, n is nodes
+//O(log n) space: height of tree
+
+function lowestCommonAncestor (root, p, q) {
+  //base case, if there is no node return null
+  //if currNode value equal to p or q's value, return current node
+  if (!root || root.val === p.val || root.val === q.val) return root
+
+  //find the LCA in root.left and root.right
+  let resL = lowestCommonAncestor(root.left, p, q)
+  let resR = lowestCommonAncestor(root.right, p, q)
+
+  //if both resL and resR have return values, meaning current root is the LCA
+  //if not both have return values, return either resL or resR that has value, or null if both p and q were not found
+  return (resL && resR) : root : (resL || resR)
+}
