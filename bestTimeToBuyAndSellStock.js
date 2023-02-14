3680ad6e09cc88d9f3b8f0944c44ac66fbb857d5
Yen Chen Lin <andylin1212@gmail.com>
2023-02-13 19:14:43 -0500

    added solution with description for best time to buy and sell stock

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -9,3 +9,20 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 //Solution
 //O(n) time: iterate through the days
 //O(1) space: no extra space
+
+function maxProfit(prices) {
+  //initialize values
+  let maxProfit = 0;
+  let currMin = prices[0]
+
+  //iterate through days starting at second day
+  for (let day = 1; day < prices.length; day++) {
+    let currPrice = prices[day]
+    //if current sell price greater than lowest buy price before current day, compare the profit to current maxProfit. Only update maxProfit if current buy/sell combination is greater
+    if (currPrice - currMin > maxProfit) maxProfit = currPrice - currMin;
+    //keep track of lowest buy price relative to current sell price
+    currMin = Math.min(currMin, currPrice)
+  }
+
+  return maxProfit;
+}
