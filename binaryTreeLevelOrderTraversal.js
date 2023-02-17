cea5ea23c7d1c744f6ea9a5d0bd03b66f11ba56e
Yen Chen Lin <andylin1212@gmail.com>
2023-02-17 17:07:42 -0500

    added description to each solution

--- binaryTreeLevelOrderTraversal.js
+++ binaryTreeLevelOrderTraversal.js
@@ -10,19 +10,24 @@ Sample output: [[3],[9,20],[15,7]]
 //O(n) space: output array has n length items
 
 function levelOrder (root) {
+  //if root === null, return empty array
   if (!root) return [];
 
+  //initialize result array and queue that has root node with corresponding level at 0
   let result = [];
   let queue = [[root, 0]]
 
+  //BFS
   while (queue.length > 0) {
     const [currNode, currLevel] = queue.shift(); //technically another O(n)
 
+    //every node we will know it's level, so push node to corresponding level array
     if (!result[currLevel]) {
       result[currLevel] = []
     }
     result[currLevel].push(currNode.val)
 
+    //add left and right (if not null) to queue while assigning it's level as currLevel + 1
     if (currNode.left) queue.push([currNode.left, currLevel + 1])
     if (currNode.right) queue.push([currNode.right, currLevel + 1])
   }
@@ -35,14 +40,22 @@ function levelOrder (root) {
 //O(n) space: output array has n length items
 
 function levelOrderTwo (root) {
+  //if root === null, return empty array
   if (!root) return [];
+
+  //initialize result array and queue with root node
   let result = []
   let queue = [root]
 
+  //BFS
   while (queue.length > 0) {
+    //set length of all nodes in this level
+    //step critical because we will be adding to queue for each node
     let qLength = queue.length;
+    //create level array
     let level = []
 
+    //iterate through the queue UP TO qLENGTH only and add values to level array
     for (let i = 1; i <= qLength; i++) {
       let currNode = queue.shift();
       level.push(currNode.val)
@@ -50,6 +63,7 @@ function levelOrderTwo (root) {
       if (currNode.left) queue.push(currNode.left)
       if (currNode.right) queue.push(currNode.right)
     }
+    //once traverse all nodes in the level, add level array to result array
     result.push(level)
   }
   return result;
