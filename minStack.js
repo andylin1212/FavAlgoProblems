78435088156eb5429d5d469c6229a36c667fb705
Yen Chen Lin <andylin1212@gmail.com>
2023-02-21 20:29:18 -0500

    adding solution 1 with stacks of object with description

--- minStack.js
+++ minStack.js
@@ -19,22 +19,30 @@ class MinStack {
     this.stack = []
   }
 
+  //O(1) time | O(1) space
   push(val) {
+    //push to stack object, storing new value and minimum so far
     this.stack.push({
       val: val,
       min: this.stack.length === 0 ? val : Math.min(val, this.getMin())
     })
   }
 
+  //O(1) time | O(1) space
   pop() {
+    this.stack.pop();
   }
 
+  //O(1) time | O(1) space
   top() {
-
+    //return .val of last element in stack
+    return this.stack[this.stack.length - 1].val
   }
 
+  //O(1) time | O(1) space
   getMin(){
-
+    //return .val of last element in stack
+    return this.stack[this.stack.length - 1].min
   }
 }
 //Solution 2: using two stacks
@@ -45,28 +53,40 @@ class MinStack {
     this.minVal = [];
   }
 
+  //O(1) time | O(1) space
   push(val) {
+    //push to main stack
     this.stack.push(val)
+
+    //if first item or value less than or equal to last item in minVal, push to minVal
     if (this.minVal.length === 0 || val <= this.minVal[this.minVal.length - 1]) {
       this.minVal.push(val)
     }
   }
 
+  //O(1) time | O(1) space
   pop() {
-    const valToPop = this.stack
+    //remove last item from main stack
+    const valToPop = this.stack.pop()
 
+    //if value removed is current min, remove from min stack
     if (this.minVal[this.minVal.length - 1] === valToPop) {
       this.minVal.pop();
     }
 
+    //return popped valu
     return valToPop
   }
 
+  //O(1) time | O(1) space
   top() {
+    //return last val in main stack
     return this.stack[this.stack.length - 1]
   }
 
+  //O(1) time | O(1) space
   getMin(){
+    //return last value in minimum value stack
     return this.minVal[this.minVal.length - 1]
   }
 }
