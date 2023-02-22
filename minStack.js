cea4e5858ee496337e3cb4951e952afbbec84191
Yen Chen Lin <andylin1212@gmail.com>
2023-02-21 20:24:38 -0500

    adding solution 2 using 2 stacks

--- minStack.js
+++ minStack.js
@@ -10,3 +10,63 @@ int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
 */
+
+
+//Solution 1: storing a stack of objects
+
+class MinStack {
+  constructor() {
+    this.stack = []
+  }
+
+  push(val) {
+    this.stack.push({
+      val: val,
+      min: this.stack.length === 0 ? val : Math.min(val, this.getMin())
+    })
+  }
+
+  pop() {
+  }
+
+  top() {
+
+  }
+
+  getMin(){
+
+  }
+}
+//Solution 2: using two stacks
+
+class MinStack {
+  constructor() {
+    this.stack = [];
+    this.minVal = [];
+  }
+
+  push(val) {
+    this.stack.push(val)
+    if (this.minVal.length === 0 || val <= this.minVal[this.minVal.length - 1]) {
+      this.minVal.push(val)
+    }
+  }
+
+  pop() {
+    const valToPop = this.stack
+
+    if (this.minVal[this.minVal.length - 1] === valToPop) {
+      this.minVal.pop();
+    }
+
+    return valToPop
+  }
+
+  top() {
+    return this.stack[this.stack.length - 1]
+  }
+
+  getMin(){
+    return this.minVal[this.minVal.length - 1]
+  }
+}
