380f13a81f933c3482a8a1fc6eedcc2226887389
Yen Chen Lin <andylin1212@gmail.com>
2023-02-24 21:17:15 -0500

    adding solution with description for implement Trie

--- implementTrie.js
+++ implementTrie.js
@@ -8,3 +8,69 @@ void insert(String word) Inserts the string word into the trie.
 boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
 boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
 */
+
+//Solution
+
+
+//supporting
+var TrieNode = function() {
+  //each node has a children Object and endOfWord boolean indicating if last char of inserted word
+  this.children = {}
+  this.endOfWord = false;
+}
+
+var Trie = function() {
+  //a trie has a root that is a TrieNode
+  this.root = new TrieNode()
+}
+
+//O(n) time: n is length of word
+//O(n) space: n is lenght of word
+Trie.prototype.insert = function(word) {
+  //initialize curr as root of trie
+  let curr = this.root
+  //iterate through all characters
+  for (let c of word) {
+    //if c not in children of current node
+    if (curr.children[c] === undefined) {
+      //assign c in current nodes children to new TreeNode
+      curr.children[c] = new TrieNode()
+    }
+    //move to children[c] node
+    curr = children[c]
+  }
+  //set last c of word as endOfWord
+  curr.endOfWord = true;
+}
+
+Trie.prototype.search = function(word) {
+  //initialize curr as root of trie
+  let curr = this.root
+  //iterate through all characters
+  for (let c of word) {
+    //if cant find c in currNode's children, return false
+    if (curr.children[c] === undefined) {
+      return false
+    }
+    //move to children[c] node
+    curr = curr.children[c]
+  }
+  //return if currentNode has been marked endOfWord, may only be a substring
+  return curr.endOfWord
+};
+
+Trie.prototype.startsWith = function(prefix) {
+  //initialize curr as root of trie
+  let curr = this.root
+  //iterate through all characters
+  for (let c of prefix) {
+    //if cant find c in currentNode's children, return false
+    if (curr.children[c] === undefined) {
+      return false
+    }
+    //move to children[c] node
+    curr = curr.children[c]
+  }
+  //found a word that has prefix as the prefix
+  return true;
+}
