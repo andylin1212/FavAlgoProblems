3b83b408f234b806f9222c9c78209411a71c0bb9
Yen Chen Lin <andylin1212@gmail.com>
2023-02-27 15:58:29 -0500

    adding description to solution 1

--- combinationSum.js
+++ combinationSum.js
@@ -10,20 +10,30 @@ The test cases are generated such that the number of unique combinations that su
 
 //Solution 1:
 function combinationSum (candidates, target) {
+  //initialize return result
   let result = [];
 
+  //define helper function
   function exploreCombo(index, target, currCombo) {
+    //base case:
+    //if target amount is less than zero there are no possible combinations so just return
     if (target < 0) return;
+    //if target amount is exactly zero, that means combination found, and add to result
     if (target === 0) result.push(currCombo)
 
+    //iterate from i = index, so values before currIndex not reused
     for (let i = index; i < candidates.length; i++) {
       const currCandidate = candidates[i]
+      //new target amount is current target amount minus current candidate
       const remainder = target - currCandidate
+      //add current candidate to currentCombo
       const newCombo = currCombo.concat(currCandidate)
+      //recursively call exploreCombo with remainder ammount and newCombo
       exploreCombo(i, remainder, newCombo)
     }
   }
 
+  //call helper function starting at index = 0 with original target and empty currCombo
   exploreCombo(0, target, [])
   return result
 };
