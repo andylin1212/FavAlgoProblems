acc970550f0d5f720c4fee4059cd87de150d8954
Yen Chen Lin <andylin1212@gmail.com>
2023-03-01 14:17:25 -0500

    adding solution

--- wordBreak.js
+++ wordBreak.js
@@ -3,3 +3,22 @@ Given a string s and a dictionary of strings wordDict, return true if s can be s
 
 Note that the same word in the dictionary may be reused multiple times in the segmentation.
 */
+
+function wordBreak (s, wordDict, startingIdx = 0, visited = {}) {
+  if (startingIdx in visited) return visited[startingIdx]
+
+  if (startingIdx === s.length) return true;
+  if (startingIdx > s.length) return false;
+
+  for (let word of wordDict) {
+    if (s.indexOf(word, startingIdx) === startingIdx) {
+      if (wordBreak(s, wordDict, startingIdx + word.length, visited)) {
+        visited[startingIdx] = true;
+        return true;
+      }
+    }
+  }
+
+  visited[startingIdx] = false;
+  return false;
+}
