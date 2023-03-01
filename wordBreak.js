5cdb3c8b32b1d6297504d1557809a6b1c27be85c
Yen Chen Lin <andylin1212@gmail.com>
2023-03-01 14:23:42 -0500

    add description to wordBreak

--- wordBreak.js
+++ wordBreak.js
@@ -5,20 +5,29 @@ Note that the same word in the dictionary may be reused multiple times in the se
 */
 
 function wordBreak (s, wordDict, startingIdx = 0, visited = {}) {
+  //if startingIdx in visited set, return saved value
   if (startingIdx in visited) return visited[startingIdx]
 
+  //if startingIdx exactly same as s length (one idx after last index of s), meaning found solution
   if (startingIdx === s.length) return true;
+  //if startingIdx greater than s.length, means didnt find word so can return false
   if (startingIdx > s.length) return false;
 
+
+  //iterate through all words
   for (let word of wordDict) {
+    //if word is found in s at startingIdx
     if (s.indexOf(word, startingIdx) === startingIdx) {
+      //recursively call wordBreak, idx changed to startingIdx + word.length which will be the startingIdx of the next word
       if (wordBreak(s, wordDict, startingIdx + word.length, visited)) {
+        //if above return true, store value at visited
         visited[startingIdx] = true;
         return true;
       }
     }
   }
 
+  //if out of loop and haven't returned true, return false
   visited[startingIdx] = false;
   return false;
 }
