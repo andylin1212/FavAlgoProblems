3833322bf4ecd7d498f6df66932be217587b07ae
Yen Chen Lin <andylin1212@gmail.com>
2023-03-04 11:31:09 -0500

    adding first iteration of solution

--- constructBinaryTreeFromPreorderAndInorderTraversal.js
+++ constructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -5,8 +5,14 @@ Given two integer arrays preorder and inorder where preorder is the preorder tra
 //Solution
 
 function buildTree (preorder, inorder) {
+  if (preorder.length === 0 || inorder.length === 0) return null;
 
+  let root = new TreeNode (preorder[0])
+  let mid = inorder.indexOf(preorder[0])
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))
 
+  return root
 }
 
 //Supporting TreeNode class
