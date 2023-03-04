a85d49767ee9ad8fe6203b8a94ef8214e2a9cd0e
Yen Chen Lin <andylin1212@gmail.com>
2023-03-03 19:44:29 -0500

    add description and time complexity for solution

--- binaryTreeRightSideView.js
+++ binaryTreeRightSideView.js
@@ -7,17 +7,27 @@ Due to rescursion property, elements on the right will update all elements on th
 */
 
 //Solution
+//O(n) time: all nodes in the tree
+//Olog n) space: levels in the tree
 function rightSideView (root) {
+  //return empty array if no trees passed in
   if (!root) return []
+
+  //initialize result array
   let result = []
 
+  //define helper function
   function pre (node, level) {
+    //base case, if node is null, simply return
     if (!node) return;
+    //set index at level in result array to node.val
     result[level] = node.val;
+    //call helper function on node.left first then node.right, when two nodes on same level, due to recursive property, element on right will update last thus overwrite previous updates
     pre(node.left, level + 1)
     pre(node.right, level + 1)
   }
 
+  //call helper function
   pre(root, 0)
   return result;
 }
