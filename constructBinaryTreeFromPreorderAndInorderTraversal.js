e49fd922811ba990a4113a104cd96d2f0ec2d617
Yen Chen Lin <andylin1212@gmail.com>
2023-03-04 12:52:26 -0500

    adding description to solution of construct binary tree from preorder and inorder traversal

--- constructBinaryTreeFromPreorderAndInorderTraversal.js
+++ constructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -5,12 +5,16 @@ Given two integer arrays preorder and inorder where preorder is the preorder tra
 //Solution
 
 function buildTree (preorder, inorder) {
+  //base case when either of the two arrays is empty, reach null node
   if (preorder.length === 0 || inorder.length === 0) return null;
 
+  //first root is first element in preorder array
   let root = new TreeNode (preorder[0])
+  //find the first element in inorder array
   let mid = inorder.indexOf(preorder[0])
-  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
-  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))
+  //pass the corresponding arrays to recursively call function for root.left and root.right
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid)) //preorder array 1st element is already used, and need to include element at mid, inorder needs to exclude element at mid since it is the root.val
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))  //preorder array start at mid + 1 till end, mid element part of root.left. Inorder needs to exclude element at mid since it is the root.val
 
   return root
 }
