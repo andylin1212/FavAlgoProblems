0235a0a6f2f4524d3c3fa5ad2be4d273aade59c5
Yen Chen Lin <andylin1212@gmail.com>
2023-03-06 12:52:37 -0500

    adding solution description for maxArea

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -8,6 +8,28 @@ Return the maximum amount of water a container can store.
 Notice that you may not slant the container.
 */
 
-function maxArea (height) {
+//Solution:
+//O(n) time:
+//O(1) space
 
+function maxArea (height) {
+  //initialize return value and supporting pointers
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    //calculate water amount with current pointers
+    let lowerHeight = Math.min(height[left], height[right])
+    let currMax = (right - left) * lowerHeight
+
+    //update the max water amount if current water amount is greater
+    if (currMax > max) max = currMax;
+
+    //update the pointer with lower height to potentially get a higher amount
+    if (height[left] < height[right]) left++
+    else right--
+  }
+
+  return max
 }
