d20664b33bd20be9fa3d43010a71ba3061cfe579
Yen Chen Lin <andylin1212@gmail.com>
2023-04-07 16:22:27 -0400

    added isAnagram solution with description and time/space complexity

--- isAnagram.js
+++ isAnagram.js
@@ -8,3 +8,28 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 //O(s + t) time: iterate through s first and then t
 //O(s) space: create an extra data object of s space
 
+function isAnagram (s, t) {
+  //if different length, definitely not anagram
+  if (s.length !== t.length) return false
+
+  //create new map
+  const map = new Map();
+
+  //for string s, store character counts in map structure
+  for (let i = 0; i < s.length; i ++) {
+    if (!map.has(s[i])) map.set(s[i], 1)
+    else map.set(s[i], map.get(s[i]) + 1)
+  }
+
+  //for string t, check if character count matches string s, when reach any inconsistency return false
+  for (let j = 0; j < t.length; j++) {
+    //if map structure doesnt have current character, return false
+    if (!map.has(t[j])) return false
+    //if curr char count is zero return false
+    if (map.get(t[j]) === 0) return false
+    //decrease current char count
+    map.set(t[j], map.get(t[j]) - 1)
+  }
+
+  return true
+}
