9fc571d173a958be7f2f809a135c6aef5b3ce853
Yen Chen Lin <andylin1212@gmail.com>
2023-04-07 15:53:37 -0400

    isAnagram set up

---
+++ isAnagram.js
@@ -0,0 +1,10 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
+
+//Solution 1:
+//O(s + t) time: iterate through s first and then t
+//O(s) space: create an extra data object of s space
+
