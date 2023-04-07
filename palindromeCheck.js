89cb7a3cc9b23b5fd064ccd1b229791d63f55d58
Yen Chen Lin <andylin1212@gmail.com>
2023-04-07 11:32:08 -0400

    adding solution 1 and 2

--- palindromeCheck.js
+++ palindromeCheck.js
@@ -3,3 +3,32 @@ A phrase is a palindrome if, after converting all uppercase letters into lowerca
 
 Given a string s, return true if it is a palindrome, or false otherwise.
 */
+
+/*
+Solution 1:
+O(n ^ 2) time: concatinating string in for loop. string manipulation is O(n) in itself
+O(n) space: new string
+*/
+
+function isPalindrome(s) {
+  let reversedString = '';
+  for (let i = s.length - 1; i >= 0; i--) {
+    reversedString += s[i]
+  }
+  return s === reversedString;
+}
+
+/*
+Solution 2:
+O(n) time: for loop
+O(n) space: new string
+*/
+
+function isPalindrome(s) {
+  let reversedChars = [];
+  for (let i = s.length - 1; i >= 0; i--) {
+    reversedChars.push(s[i])
+  }
+
+  return s = reversedChars.join('')
+}
