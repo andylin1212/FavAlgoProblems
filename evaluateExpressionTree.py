c4a665bce3689e00f289daaf962d67116159d516
Yen Chen Lin <andylin1212@gmail.com>
2023-04-11 19:14:04 -0400

    setup evaluate expression tree question

---
+++ evaluateExpressionTree.py
@@ -0,0 +1,7 @@
+"""
+Given a binary expression tree. Write a function to evaluate this tree mathematically and return a single resulting integer
+
+All leaf nodes in tree represent operands, which will always be positive integers. All the other nodes represent operands, which will always be positive integers. All of the other nodes represents operators, each will be a negative integer
+
+-1 = Addition: adding left and right subtrees
+"""
