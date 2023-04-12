69f16ba71a414ecf4be19d7934937249b8915429
Yen Chen Lin <andylin1212@gmail.com>
2023-04-12 13:10:25 -0400

    bestDigits question setup

---
+++ bestDigits.py
@@ -0,0 +1,14 @@
+"""
+Write a function that takes a positive integer represented as a string "number" and an integer "numDigits". Remove numDigits from the string so that the number represented by string is as large as possible afterwards
+
+Note that the order of the remaining digits cannot be changed. You can assume numDigits will always be less than the length of number and greater than or equal to 0
+
+Sample:
+Input:
+number =  "462839"
+numDigits = 2
+
+Output:
+"6839"
+"""
+
