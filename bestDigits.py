f6f9a96ebe16d782009dccda176486490a7d1aab
Yen Chen Lin <andylin1212@gmail.com>
2023-04-12 13:19:50 -0400

    adding solution with description and time/space complextiy for bestDigits

--- bestDigits.py
+++ bestDigits.py
@@ -12,3 +12,26 @@ Output:
 "6839"
 """
 
+#Solution:
+#O(n) time: n is length of number
+#O(n) space: n is length of number, max length of stack
+
+def bestDigits(number, numDigits):
+  stack = []
+
+  #iterate through all number
+  for digit in number:
+    #if there are still digits to remove and there are items on stack, check if last item on stack less than current digit, if it is remove last item on stack
+    while numDigits > 0 and len(stack) > 0 and digit > stack[len(stack) - 1]:
+      numDigits -= 1
+      stack.pop()
+    #add current digit to stack
+    stack.append(digit)
+
+  #might be situation where all numbers are added, but not enough numbers removed
+  #remove from end of stack (the lowest numbers)
+  while numDigits > 0:
+    numDigits -= 1
+    stack.pop()
+
+  return "".join(stack)
