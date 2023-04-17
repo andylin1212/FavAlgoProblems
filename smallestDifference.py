f050839ea67e1fc56fc4147df868cfaf921ad59e
Yen Chen Lin <andylin1212@gmail.com>
2023-04-17 11:57:36 -0400

    solution with description and time/space complexity

--- smallestDifference.py
+++ smallestDifference.py
@@ -5,3 +5,39 @@ Note that the absolute difference of two integers is the distance between them o
 
 You can assume that there will only be one pair of numbers with the smallest difference
 """
+
+#Solution: two pointer after sorting array
+#O(nlog(n) + mlog(m)) time: sorting both arrays
+#O(1) space: different variables needed but no other DS needed
+def smallestDifference(arrayOne, arrayTwo):
+  #sort the input arrays
+  arrayOne.sort()
+  arrayTwo.sort()
+
+  #assign initial values for index pointers, smallest difference, and smallestPair
+  idxOne = 0
+  idxTwo = 0
+  smallest = float("inf")
+  current = float("inf")
+  smallestPair = []
+
+  #starting at both index = 0, for every pair of the sorted array, we explore all possible solutions by finding if number at arrayOne or arrayTwo is smaller, then increasing the index of the smaller number for next possible pair
+  while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+    firstNum = arrayOne[idxOne]
+    secondNum = arrayTwo[idxTwo]
+
+    if firstNum < secondNum:
+      current = secondNum - firstNum
+      idxOne += 1
+    elif secondNum < firstNum:
+      current = firstNum - secondNum
+      idxTwo += 1
+    else:
+      return [firstNum, secondNum]
+
+    #update smallest difference and smallest pair if current different is smaller than smallest difference thus far
+    if smallest > current:
+      smallest = current
+      smallestPair = [firstNum, secondNum]
+
+  return smallestPair
