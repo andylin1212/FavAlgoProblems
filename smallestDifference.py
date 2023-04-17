6550723f03d7c5df410dfab58df3700f52ed8d3b
Yen Chen Lin <andylin1212@gmail.com>
2023-04-17 11:22:08 -0400

    smallestDifference problem set up

---
+++ smallestDifference.py
@@ -0,0 +1,7 @@
+"""
+Write a function that takes in two non-empty arrays of integers, find the pair of numbers (one from each array) whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from the first array in the first position
+
+Note that the absolute difference of two integers is the distance between them one the real number line. For example, absolute different of -5 and 5 is 10, and the absolute different of -5 and -4 is 1.
+
+You can assume that there will only be one pair of numbers with the smallest difference
+"""
