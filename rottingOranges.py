ca78e17e18c96be0bad25ab64b0258bd702d72a3
Yen Chen Lin <andylin1212@gmail.com>
2023-05-11 13:01:07 -0400

    provide rotting oranges solution with explanation

--- rottingOranges.py
+++ rottingOranges.py
@@ -9,3 +9,38 @@ Every minute, any fresh orange that is 4-directionally adjacent to a rotten oran
 Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 '''
 
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque() #create a queue
+
+        #initialize variables
+        time, fresh = 0, 0
+        maxRow, maxCol = len(grid), len(grid[0])
+
+        #prework , to update fresh tomatoes and add rotten tomatoes to queue
+        for r in range(maxRow):
+            for c in range(maxCol):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append([r, c])
+
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        #rotten tomatoes by batch and count up the minutes
+        #exit rotten tomatoes process when there are no more rotten tomatoes that hasn't been processed or there are no more fresh tomatoes
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    #check if in bounds
+                    if not (0 <= nr < maxRow and 0 <= nc < maxCol):
+                        continue
+                    #no need to process if its not a fresh tomatoe ready to be rotten
+                    if grid[nr][nc] != 1:
+                        continue
+                    grid[nr][nc] = 2
+                    q.append([nr, nc])
+                    fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
